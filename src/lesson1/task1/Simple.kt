4549aaf21eedae68fe2f5aa773f5018952d4260b
Igor <igordyadlev@gmail.com>
2022-09-18 13:22:07 +0300

    7

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -136,4 +136,9 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    val x1 = number % 10
+    val x2 = number / 10 % 10
+    val x3 = number / 10 / 10 % 10
+    return x1 * 100 + x2 * 10 + x3
+}
